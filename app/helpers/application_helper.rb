92a0a3cf940231a6708d597f99267904ebdcbdf1
Chris Thompson <chris@goclio.com>
2016-08-29 12:37:40 -0600

    Add first name, last name to user registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def username(user)
+    "<b>#{user.first_name} #{user.last_name}</b>".html_safe
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518185634) do
+ActiveRecord::Schema.define(version: 20160829172105) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20150518185634) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
